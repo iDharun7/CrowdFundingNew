5f67a78d2ec216f6cbf5a56a437c9dc37cab1452
iDharun7 <dreampalace7777@gmail.com>
2023-04-30 12:52:58 +0530

    done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ThirdwebProvider desiredChainId = {ChainId.Sepolia}>
+    <ThirdwebProvider desiredChainId = {ChainId.Goerli}>
         <Router>
             <App/>
         </Router>
