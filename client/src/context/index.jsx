de2bb5bdbbd501ec390f23c0491dbe2e34bf06b1
iDharun7 <dreampalace7777@gmail.com>
2023-05-12 21:57:35 +0530

    done

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x017b7009967fE3d101acf983bFD881105991Df23');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
