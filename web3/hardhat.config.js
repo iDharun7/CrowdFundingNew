dff1a61851e4d27d2a8278a8b1825403ac76dc17
iDharun7 <dreampalace7777@gmail.com>
2023-05-12 23:36:12 +0530

    done

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -3,11 +3,11 @@ module.exports = {
   solidity: {
     version: '0.8.9',
     defaultNetwork: 'sepolia',
-    networks:{
-      hardhat:{},
-      sepolia:{
-        URL: 'https://rpc.ankr.com/eth_sepolia',
-        accounts: ['0x${process.env.PRIVATE_KEY}']
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: 'https://rpc.ankr.com/eth_sepolia',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
     settings: {
